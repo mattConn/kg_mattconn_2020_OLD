745d4104d97d19aacc0d646726c2f817b7116b85
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-03-14 03:41:52 -0400

    removed temp variable for storing set cast of string in 1to1 function, now casting in-place

--- isOneToOne.py
+++ isOneToOne.py
@@ -6,11 +6,7 @@ def isOneToOne(s1, s2):
         return False
 
     # if no duplicate characters exist in s1, then a 1-to-1 mapping is possible (no duplicate mappings)
-    
-    # remove duplicate characters from s1
-    temp = set(s1)
-    #  if length is the same, no duplicates were present
-    if len(temp) == len(s1):
+    if len(set(s1)) == len(s1):
         return True 
     else:
        return False 
